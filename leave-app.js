2ed4e209cac2d660804cf31cc39a7e969061aea5
Im Code <141347375+Thecode764@users.noreply.github.com>
2023-09-15 14:38:41 +0330

    Create leave-app.js

---
+++ leave-app.js
@@ -0,0 +1,14 @@
+function showYesNoCancelAlert(message) {
+    var result = confirm(message);
+    if (result) {
+      alert("Go To Clock");
+      document.getElementById("demo").style.color = "green";
+      document.getElementById("demo").innerHTML = "green";
+    } else if (result === false) {
+      alert("Changed To Thecode746 Home Page");
+      window.location.href = "https://thecode764.github.io";
+    } else {
+      alert("Changed To Home Page");
+    }
+  }
+  showYesNoCancelAlert("Go To New Page?")
