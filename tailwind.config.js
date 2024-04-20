af1111437c63584420960da1e08bdc4a794b619a
Artin karimi <141347375+Thecode764@users.noreply.github.com>
2024-04-20 16:03:30 +0330

    Add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      height: {
+        '100vh': '100vh'
+      },
+    },
+  },
+  plugins: [],
+}
